7bf658f3c611e79537aeac84759d941136909116
Ferel <66280242+FerelUltra@users.noreply.github.com>
2025-03-05 16:07:48 +0300

    feat: add prefixes to errors

--- cron.go
+++ cron.go
@@ -33,12 +33,12 @@ func parsePart(part string, startValue, maxValue int) (string, error) {
 
 		start, err := strconv.Atoi(hyphenSplits[0])
 		if err != nil {
-			return "", fmt.Errorf("conversion error start_value: %v", err)
+			return "", fmt.Errorf("parsePart: failed to convert start_value: %w", err)
 		}
 
 		end, err := strconv.Atoi(hyphenSplits[1])
 		if err != nil {
-			return "", fmt.Errorf("conversion error end_value: %v", err)
+			return "", fmt.Errorf("parsePart: failed to convert end_value: %w", err)
 		}
 
 		for i := start; i <= end; i++ {
@@ -51,7 +51,7 @@ func parsePart(part string, startValue, maxValue int) (string, error) {
 		slashSplits := strings.Split(part, "/")
 		step, err := strconv.Atoi(slashSplits[1])
 		if err != nil {
-			return "", fmt.Errorf("conversion error step value: %v", err)
+			return "", fmt.Errorf("parsePart: failed to convert step value: %w", err)
 		}
 
 		for i := startValue; i <= maxValue; i += step {
@@ -70,7 +70,7 @@ func printPart(width, index, startValue, maxValue int, name string, parts []stri
 	} else {
 		parsed, err := parsePart(parts[index], startValue, maxValue)
 		if err != nil {
-			return err
+			return fmt.Errorf("printPart: error parsing %q: %w", name, err)
 		}
 		fmt.Printf("%-*s %s\n", width, name, parsed)
 	}
@@ -92,7 +92,7 @@ func cron(str string) error {
 	for index, t := range timeSlice {
 		err := printPart(width, index, t.startValue, t.maxValue, t.name, parts)
 		if err != nil {
-			return err
+			return fmt.Errorf("cron: failed at %q: %w", t.name, err)
 		}
 	}
 
